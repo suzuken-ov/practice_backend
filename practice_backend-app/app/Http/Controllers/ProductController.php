15104aae476b21657c8634ecb797d1d24319b3f9
Chamuji <s1412052@s.do-johodai.ac.jp>
2023-12-05 19:57:28 +0900

    php.apiとの疎通

---
+++ practice_backend-app/app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    public function showProductPage(){
+        //return view('product');
+        return "hoge";
+    }
+}
--- practice_backend-app/routes/api.php
+++ practice_backend-app/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// /products URL
+//ProdcurContollerで定義したshowProductPage関数
+Route::get('/products', [ProductController::class, 'showProductPage']);
